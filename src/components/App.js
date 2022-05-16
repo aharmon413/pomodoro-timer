7c191b3e6f17fd3cb6f98fbdd3b54b2c184526db
Amber Harmon <amber@amberharmon.com>
2022-05-16 14:26:56 -0400

    Add favicons and GitHub link

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Timer from "./Timer";
 import Quote from "./Quote";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="app">
       <Timer />
       <Quote />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="footer">
+            project by amber harmon - <a href="https://github.com/aharmon413/pomodoro-timer" target="_blank">view on github</a>
+        </div>
+     );
+}
+ 
+export default Footer;
